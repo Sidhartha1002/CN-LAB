157a5a10cd31a5459816f7fd2ca53ab9fcfb7db2
Sidhartha1002 <134572048+Sidhartha1002@users.noreply.github.com>
2023-08-01 09:44:13 +0530

    Update CN_Lab_1-Q5.C

--- CN_Lab_1-Q5.C
+++ CN_Lab_1-Q5.C
@@ -1,73 +1,36 @@
 #include <stdio.h>
 
-// Union to store the number and its bytes in different ways
-union Number {
-    int num;
-    char bytes[sizeof(int)];
-};
-
-// Function to check the Endianness of the host machine
-int isLittleEndian() {
-    union Number n;
-    n.num = 1;
-    return n.bytes[0];
+void print_bytes(char *data, int size) {
+    printf("Bytes: ");
+    for (int i = 0; i < size; i++) {
+        printf("%02X ", (unsigned char)data[i]);
+    }
+    printf("\n");
 }
 
-// Function to convert Endianness from Little to Big or vice versa
-void convertEndianness(int *num) {
-    union Number n;
-    n.num = *num;
-    char temp;
-
-    if (isLittleEndian()) {
-        // Convert Little Endian to Big Endian
-        temp = n.bytes[0];
-        n.bytes[0] = n.bytes[3];
-        n.bytes[3] = temp;
-
-        temp = n.bytes[1];
-        n.bytes[1] = n.bytes[2];
-        n.bytes[2] = temp;
-    } else {
-        // Convert Big Endian to Little Endian
-        temp = n.bytes[0];
-        n.bytes[0] = n.bytes[3];
-        n.bytes[3] = temp;
-
-        temp = n.bytes[1];
-        n.bytes[1] = n.bytes[2];
-        n.bytes[2] = temp;
+void swap_endianness(char *data, int size) {
+    for (int i = 0; i < size / 2; i++) {
+        char temp = data[i];
+        data[i] = data[size - 1 - i];
+        data[size - 1 - i] = temp;
     }
-
-    *num = n.num;
 }
 
 int main() {
     int num;
-
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Print the content of each byte location
-    union Number n;
-    n.num = num;
-    printf("Content of each byte location:\n");
-    for (int i = 0; i < sizeof(int); i++) {
-        printf("Byte %d: %02X\n", i, n.bytes[i]);
-    }
+    char *bytes = (char *)&num;
+    int size = sizeof(num);
 
-    // Check the Endianness of the host machine
-    if (isLittleEndian()) {
-        printf("Host machine is in Little Endian.\n");
-    } else {
-        printf("Host machine is in Big Endian.\n");
-    }
+    printf("Endianness: %s Endian\n", (bytes[0] == 0) ? "Big" : "Little");
+    print_bytes(bytes, size);
 
-    // Convert the Endianness of the number
-    convertEndianness(&num);
+    swap_endianness(bytes, size);
 
-    // Print the converted number
-    printf("Converted number: %d\n", num);
+    printf("Converted Endianness: %s Endian\n", (bytes[0] == 0) ? "Big" : "Little");
+    print_bytes(bytes, size);
 
     return 0;
 }
